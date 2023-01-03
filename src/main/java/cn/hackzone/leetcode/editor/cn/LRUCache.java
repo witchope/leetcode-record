9067b06e7f1204ca0bad439beffc5e030c4b080b
guoxiaohan <guoxiaohan@rjmart.cn>
2023-01-03 10:02:54 +0800

    feat: LRU 缓存

---
+++ src/main/java/cn/hackzone/leetcode/editor/cn/LRUCache.java
@@ -0,0 +1,96 @@
+package cn.hackzone.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class LRUCache {
+    static class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+
+        public DLinkedNode() {
+        }
+
+        public DLinkedNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            ++size;
+            if (size > capacity) {
+                DLinkedNode tail = rmTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+
+    }
+    private void moveToHead(DLinkedNode node) {
+        rmNode(node);
+        addToHead(node);
+    }
+
+    private void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void rmNode(DLinkedNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private DLinkedNode rmTail() {
+        DLinkedNode node = tail.prev;
+        rmNode(node);
+        return node;
+    }
+
+    public static void main(String[] args) {
+
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // 缓存是 {1=1}
+        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        lRUCache.get(1);    // 返回 1
+        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        lRUCache.get(2);    // 返回 -1 (未找到)
+        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        lRUCache.get(1);    // 返回 -1 (未找到)
+        lRUCache.get(3);    // 返回 3
+        lRUCache.get(4);    // 返回 4
+    }
+}
