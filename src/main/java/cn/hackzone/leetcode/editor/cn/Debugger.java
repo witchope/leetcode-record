c727f8b3d92377a69d497b086c7337fc41284021
guoxiaohan <guoxiaohan@rjmart.cn>
2023-02-02 17:20:08 +0800

    feat: 增加 Debugger 工具类

---
+++ src/main/java/cn/hackzone/leetcode/editor/cn/Debugger.java
@@ -0,0 +1,17 @@
+package cn.hackzone.leetcode.editor.cn;
+
+import cn.hackzone.leetcode.editor.common.struct.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Debugger {
+
+    public static void main(String[] args) {
+    }
+
+    static
+    class Solution {
+    }
+
+}
